c883279a6f3483da57d753ef42c123e461e70c6b
alkaalvinn <alkaalvinn@student.ub.ac.id>
2024-12-02 15:03:26 +0700

    Add Particles

--- src/components/particles.tsx
+++ src/components/particles.tsx
@@ -1,37 +1,24 @@
-
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import type { Engine, ISourceOptions } from "@tsparticles/engine"; // Ubah impor tipe dari modul ini
+import { loadSlim } from "@tsparticles/slim";
 
+interface ParticlesComponentProps {
+  id: string;
+}
 
-
-const ParticlesComponent = (props) => {
-
+const ParticlesComponent: React.FC<ParticlesComponentProps> = (props) => {
   const [init, setInit] = useState(false);
-  // this should be run only once per application lifetime
+
   useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
+    initParticlesEngine(async (engine: Engine) => {
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
   }, []);
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
-
-  const options = useMemo(
+  const options: ISourceOptions = useMemo(
     () => ({
       fpsLimit: 120,
       interactivity: {
@@ -42,7 +29,7 @@ const ParticlesComponent = (props) => {
           },
           onHover: {
             enable: true,
-            mode: 'grab',
+            mode: "grab",
           },
         },
         modes: {
@@ -94,11 +81,10 @@ const ParticlesComponent = (props) => {
       },
       detectRetina: true,
     }),
-    [],
+    []
   );
 
-
-  return <Particles id={props.id} init={particlesLoaded} options={options} />; 
+  return init ? <Particles id={props.id} options={options} /> : null;
 };
 
 export default ParticlesComponent;
